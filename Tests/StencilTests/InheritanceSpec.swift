5e15861d9bd9a01743f91200f511f09e8acc700a
Hemet <hemet.mail@gmail.com>
2021-03-27 16:59:00 +0400

    Fix tests that use multi-line string literals

--- Tests/StencilTests/InheritanceSpec.swift
+++ Tests/StencilTests/InheritanceSpec.swift
@@ -15,6 +15,7 @@ final class InheritanceTests: XCTestCase {
         Super_Header Child_Header
         Child_Body
         """
+        .withPlatformNewLine
     }
 
     it("can inherit from another template inheriting from another template") {
@@ -23,6 +24,7 @@ final class InheritanceTests: XCTestCase {
         Super_Header Child_Header Child_Child_Header
         Child_Body
         """
+        .withPlatformNewLine
     }
 
     it("can inherit from a template that calls a super block") {
@@ -31,6 +33,24 @@ final class InheritanceTests: XCTestCase {
         Header
         Child_Body
         """
+        .withPlatformNewLine
     }
+  }
+}
+
+private extension String {
+  /*
+  Multi-line literals use \n new lines on all Platforms.
+  However, by default Git converts new lines of all text files
+  in working copy to the platform default which is \r\n for Windows.
+  Since the test templates are affected we need to address that
+  by making string literals use platform-specific new lines.
+  */
+  var withPlatformNewLine: String {
+    #if os(Windows)
+      return replacingOccurrences(of: "\n", with: "\r\n")
+    #else
+      return self
+    #endif
   }
 }
